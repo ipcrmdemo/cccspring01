2e72a01cbecf12915827719ac4b55fb1a590fc4a
Matt Cadorette <mcadorette@gmail.com>
2019-01-10 10:28:56 -0500

    Initial commit from Atomist

---
+++ src/main/java/com/cccspring01/Cccspring01Application.java
@@ -0,0 +1,12 @@
+package com.cccspring01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cccspring01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cccspring01Application.class, args);
+	}
+}
